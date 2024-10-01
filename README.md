d218b5bf350122415c06a43ac3cb9fc207bd9c32
Thiago Vieira Camara <mustache301@gmail.com>
2024-10-01 09:41:22 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple CLI calculator
+
+Simple CLI calculator made with typer library and rich, with the intent to learn how to make cli scripts.
+
---
+++ calculator.py
@@ -0,0 +1,27 @@
+import typer
+from rich import print as println
+from typing_extensions import Annotated, Callable
+
+def calculator(a: Annotated[float, typer.Argument(help="1st value", show_default=False)],
+                b: Annotated[float, typer.Argument(help="2nd value", show_default=False)],
+                minus: Annotated[bool, typer.Option("-m", "--minus",help="Subtraction operation", show_default=False)] = False, 
+                times: Annotated[bool, typer.Option("-mt", "--mult",help="Multiplication operation", show_default=False)] = False, 
+                divide:Annotated[bool, typer.Option("-dv", "--divide",help="Division operation", show_default=False)] = False) -> float:
+    """
+    Simple mafs, just enter two values (a and b), default is sum.
+    """
+    if minus:
+        subtract = a - b
+        return println(f"[green]{subtract}[green]")
+    elif times:
+        multiplication = a * b
+        return println(f"[blue]{multiplication}[blue]")
+    elif divide:
+        divided = a / b
+        return println(f"[red]{divided}[red]")
+    else:
+        sum = a+b
+        return println(f"[yellow]{sum}[yellow]")
+
+if __name__ == "__main__":
+    typer.run(calculator)
